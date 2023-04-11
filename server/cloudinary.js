2638d147b61d07b62a9f1012e15cba7145e261e8
Joshua Lehman <joshlehman.dev@gmail.com>
2023-04-11 11:09:58 -0400

    Fix typo with CLOUDINARY_API_SECRET
    Was CLOUDINARY_API_SERCRET

--- server/cloudinary.js
+++ server/cloudinary.js
@@ -10,7 +10,7 @@ dotEnv.config();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SERCRET,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 /*
